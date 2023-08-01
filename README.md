9085ca6a28553d4754d74f6b2cda12a8e2ecfd29
aofei.li <aofei.li@pacteraedge.com>
2023-08-01 22:01:54 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# fast-console
-vscode快速插入打印语句插件
+# Fast-Console
+一个通过快捷键快速插入打印语句的插件,目前只支持最常用的两种: `console.log`和`console.dir`,
+使用时会在指定位置的上一行或下一行插入:`console.log/dir("$1:",$1)`,
+## 使用步骤
++ 选中要打印的变量 or 函数 or anything(未选中时插入空的打印语句)
++ 按中组合键:`alt+1`,表示在选中位置上一行插入打印语句`console.log("$1:",$1)`(Mac为`cmd+1`)
++ 按中组合键:`alt+2`,表示在选中位置下一行插入打印语句`console.log("$1:",$1)`(Mac为`cmd+2`)
++ 按中组合键: `alt+shift+1`,表示在选中位置上一行插入语句`console.dir("$1:",$1)`(Mac为`cmd+shift+1`)
++ 按中组合键: `alt+shift+2`,表示在选中位置下一行插入语句`console.dir("$1:",$1)`(Mac为`cmd+shift+2`)
---
+++ extension.js
@@ -0,0 +1,74 @@
+const vscode = require('vscode');
+
+/**
+ * @message: 插件激活时触发
+ * @param {vscode.ExtensionContext} context
+ * @since: 2023-08-01 16:16:33
+ */
+function activate(context) {
+	context.subscriptions.push(
+    vscode.commands.registerCommand('fast-console.log.nextline',()=> addConsole()),
+    vscode.commands.registerCommand('fast-console.log.prevline',()=>addConsole("log",0)),
+    vscode.commands.registerCommand('fast-console.dir.nextline',()=> addConsole("dir")),
+    vscode.commands.registerCommand('fast-console.dir.prevline',()=> addConsole("dir",0))
+  );
+}
+
+function deactivate() {}
+
+/**
+ * @message: 插入console字符串
+ * @param {*} type console类型
+ * @param {*} line 插入位置偏移量
+ * @since: 2023-08-01 18:29:32
+ */
+function addConsole(type = "log",line = 1){
+  const editor = vscode.window.activeTextEditor;
+  if(!editor) return
+  const selection = editor.selection;
+  const selectionText = editor.document.getText(selection);
+
+  // 创建代码片段
+  let str
+  if(selectionText === ""){
+    str = `console.${type}();`
+  }else {
+    str = `console.${type}("${selectionText}:",${selectionText});`
+  }
+  const snippet = new vscode.SnippetString(str);
+
+  // 设置插入片段的位置
+  const prevPos = editor.selection.active;
+  const nextPos = new vscode.Position(prevPos.line+line, 0)
+
+  // 插入代码片段
+  editor.insertSnippet(snippet,nextPos).then(() => {
+    vscode.commands.executeCommand('type', {
+        text: '\n',
+    });
+    cursorMove("left",3)
+  });
+}
+
+/**
+ * @message: 将光标移动
+ * @param {*} direction 方向
+ * @param {*} distance 步长
+ * @since: 2023-08-01 18:23:36
+ */
+function cursorMove(direction,distance = 1){
+  for(let i = 0; i < distance; i++){
+    vscode.commands.executeCommand('cursorMove', {
+      to: direction, // 向右移动一个位置
+      by: 'character', // 移动的单位为字符
+      value: 1, // 移动的数量为1个字符
+      select: false, // 不选中文本
+    });
+  }
+}
+
+
+module.exports = {
+	activate,
+	deactivate
+}
